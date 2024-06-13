42e34098316c9d4344e83ade7b0fe1d8a2de24cc
Maja Frankowska <majafrankowska88@gmail.com>
2024-06-13 23:28:24 +0200

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "pink background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at Polish-Japanese Academy of Information Technology. I enjoy creating interesting and creative apps.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Software Engineering",
+  "Front end",
+  "Back end",
+  "Data structures & Algorithms",
+  "Data base design",
+  "App Development",
+
 ];
 
 /**
@@ -45,8 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+"I am passionate about solving complex problems through innovative solutions. Leveraging my background in computer science, I continually seek new and efficient ways to develop technology that is accessible and user-friendly for everyone.";
 const About = () => {
   return (
     <section className="padding" id="about">
