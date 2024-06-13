ebd269c1eadc3111e9ef31eff092aa9e0ed95860
Maja Frankowska <majafrankowska88@gmail.com>
2024-06-13 23:34:32 +0200

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,7 +21,7 @@ import React from "react";
  */
 import image from "../images/design-desk.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desktop with a laptop";
 
 /**
  * Project list
@@ -31,28 +31,10 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Interview with me about my journey as a woman in STEM",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+     "Growing up, I developed a passion for science and technology, which led me to pursue programming and a career that contributes to societal progress. I am committed to motivating young girls to believe in themselves and break free from stereotypes, proving that any STEM or technical field is within their reach.",
+    url: "https://www.dziewczynynapolitechniki.pl/ambasadorki-art/273-plec-nie-determinuje",
   },
 ];
 
