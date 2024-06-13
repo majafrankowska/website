10d7841df936b6a29d3518b4f67b1ab686bb800e
Maja Frankowska <majafrankowska88@gmail.com>
2024-06-13 21:52:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Maja Frankowska",
+  title: "Computer Science Student & Software Engineer",
+  email: "majafrankowskawork@gmail.com",
+  gitHub: "majafrankowska",
+  instagram: "majafrn",
+  linkedIn: "majafrankowska",
+  medium: "majafrankowska",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
